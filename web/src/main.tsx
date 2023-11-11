a194cc057f81b0a472a7a82e74dd9ff9eb88df4b
bobi <boqn.damqnov03@gmail.com>
2023-11-11 15:14:49 +0200

    dark mode added

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "./utils/themeProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider defaultTheme="system" storageKey="ui-theme">
     <App />
-  </React.StrictMode>,
-)
+  </ThemeProvider>
+);
